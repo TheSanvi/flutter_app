f15d448ddcd48820ccaa9b063be7340d444b90f8
TheSanvi <sanvivermavns@gmail.com>
2023-06-18 13:06:46 +0530

    basics of dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Sanvi";
+    double pi = 3.14;
+    bool isFemale = true;
+    num temp = 30.4;
+
+
+    var day = "Sunday";
+    const pi = 3.14;
+
     return MaterialApp(
       home: Material(
         child: Center(
           child: Container(
-            child: Text("Welcome 30 days of flutter"),
+            child: Text("Welcome $days of flutter of name $name"),
           ),
         ),
       ),
