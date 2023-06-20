18e144fae1fcbd6b5d3b02eb806683fb0e711287
TheSanvi <sanvivermavns@gmail.com>
2023-06-20 11:31:15 +0530

    image added
    day 4 (asset image added ,goggle fonts added)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/page/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_app/page/login_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,21 +13,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // home :HomePage(),
-      themeMode: ThemeMode.light ,
-      theme: ThemeData(primarySwatch:Colors.purple),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      fontFamily:GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-       initialRoute: "/home",
-       routes:{
-        "/":(context)=> LoginPage(),
-         "/home":(context)=> HomePage(),
-         "/login":(context)=>LoginPage()
-
-    },
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+        "/login": (context) => const LoginPage()
+      },
     );
   }
 }
--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -6,15 +6,25 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 20,
-              color: Colors.blueAccent,
-              fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_img.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        )));
   }
 }
